75c6b57219886be8040d07df2a0a93207e986ad7
Manas <devmanas45@gmail.com>
2025-02-04 13:02:59 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ButtonGradient from './assets/svg/ButtonGradient';
+import { Button } from './components/Button';
+import Header from './components/Header';
+import { Outlet } from 'react-router-dom';
+import Hero from './components/Hero';
+import Benefits from './components/Benefits';
+import Collaboration from './components/Collaboration';
+import Services from './components/Services';
+import Pricing from './components/Pricing';
+import Roadmap from './components/Roadmap';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <>
+    
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+        <Header/>
+        <Hero/>
+        <Benefits/>
+        <Collaboration/>
+        <Services/>
+        <Pricing/>
+        <Roadmap/>
+        <Footer/>
+                {/* <Outlet/> */}
+      </div>
+      <ButtonGradient/>
+      
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, BrowserRouter as Router, RouterProvider } from 'react-router-dom'
+// const router=createBrowserRouter(
+//   createRoutesFromElements(
+//     < Route path='/' element={<App/>}>
+//       <Route/>
+//     </Route>
+//   )
+// )
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <RouterProvider router={router}/> */}
+    <Router>
+      <App/>
+    </Router>  {/*//USED WITH BROWSER ROUTER*/ }
+  </StrictMode>,
+)
