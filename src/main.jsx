d99cce515041cb0d48ea8deac4befefa17bddd96
Manas <devmanas45@gmail.com>
2025-02-04 17:46:29 +0530

    main.jsx BrowserRouter changed to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, createRoutesFromElements, Route, BrowserRouter as Router, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, HashRouter as Router, RouterProvider } from 'react-router-dom'
 // const router=createBrowserRouter(
 //   createRoutesFromElements(
 //     < Route path='/' element={<App/>}>
